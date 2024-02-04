305b9669f935981904fbe68418a1c1c489c45d12
AlDjuric <aleksandar.djuriic@gmail.com>
2024-02-04 17:53:35 +0100

    fixed sqrt

---
+++ HLS/config.h
@@ -0,0 +1,27 @@
+/**
+ * @file config.h
+ * @author Aleksandar Djuric
+ * @brief Configuration file for the Sobel operator
+ * @version 0.1
+ * @date 2024-02-04
+ *
+ * @copyright Check the LICENSE file for details
+ *
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// includes
+#include "HLS/hls.h"
+#include "HLS/ac_fixed.h"
+#include "HLS/ac_fixed_math.h"
+#include "stdio.h"
+
+// defines
+#define HEIGHT 5
+#define WIDTH  5
+
+// typedefs
+
+#endif
